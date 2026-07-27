6101a5f84530aa12cff2728f347d9d8470518b8d
John Christopher Cruz <hi@notjc.dev>
2026-07-27 20:13:03 +0800

    Initial Commit

---
+++ Aligna/AlignaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlignaApp.swift
+//  Aligna
+//
+//  Created by John Christopher Cruz on 7/27/26.
+//
+
+import SwiftUI
+
+@main
+struct AlignaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlignaTests/AlignaTests.swift
@@ -0,0 +1,19 @@
+//
+//  AlignaTests.swift
+//  AlignaTests
+//
+//  Created by John Christopher Cruz on 7/27/26.
+//
+
+import Testing
+@testable import Aligna
+
+struct AlignaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
